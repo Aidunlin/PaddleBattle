28e642486cb9e4549b4f9997a33f6e20701cd9b3
Aidunlin <aidunlin@gmail.com>
2020-05-29 14:55:16 -0700

    Implement main menu and gamestate variables

--- Main.gd
+++ Main.gd
@@ -1,79 +1,129 @@
 extends Node2D
 
-export var total_health = 4
-export var max_players = 8
-
-onready var camera = get_node("Camera2D")
-onready var players = get_node("Players")
-onready var balls = get_node("Balls")
+onready var main_menu = "UILayer/Menu/Main/"
+onready var message = get_node("UILayer/Message")
+onready var camera = get_node("Game/Camera2D")
+onready var players = get_node("Game/Players")
 onready var bars = get_node("UILayer/HUD/Bars")
-onready var player_spawns = get_node("TestMap/PlayerSpawns").get_children()
-onready var ball_spawns = get_node("TestMap/BallSpawns").get_children()
+onready var player_spawns = get_node("Game/TestMap/PlayerSpawns")
 
+var playing = false
+var started = false
+var total_hp = 3
+var max_players = 4
+var max_balls = 10
 var player_db = []
 var used_colors = []
-var zoom_threshold = 500
-var zoom_acceleration = 0.05
+var zoom_margin = 500
+var zoom_accel = 0.05
 
 func _ready():
 	OS.min_window_size = Vector2(1280, 720)
-	camera.position = get_node("TestMap/DefCamPos").position
-	for spawn in ball_spawns:
+	get_node(main_menu + "Play").connect("pressed", self, "load_game")
+	get_node(main_menu + "Quit").connect("pressed", get_tree(), "quit")
+	get_node(main_menu + "Health/Inc").connect("pressed", self, "crement_health", [1])
+	get_node(main_menu + "Health/Dec").connect("pressed", self, "crement_health", [-1])
+	get_node(main_menu + "Players/Inc").connect("pressed", self, "crement_players", [1])
+	get_node(main_menu + "Players/Dec").connect("pressed", self, "crement_players", [-1])
+	get_node(main_menu + "Balls/Inc").connect("pressed", self, "crement_balls", [1])
+	get_node(main_menu + "Balls/Dec").connect("pressed", self, "crement_balls", [-1])
+	get_node("Game/ResetTimer").connect("timeout", self, "unload_game")
+	
+	var save_data = File.new()
+	if save_data.file_exists("user://save.txt"):
+		save_data.open("user://save.txt", File.READ)
+		total_hp = int(save_data.get_line())
+		max_players = int(save_data.get_line())
+		max_balls = int(save_data.get_line())
+		save_data.close()
+	get_node(main_menu + "Health/HealthNum").text = str(total_hp)
+	get_node(main_menu + "Players/PlayerNum").text = str(max_players)
+	get_node(main_menu + "Balls/BallNum").text = str(max_balls)
+
+# Increment/decrement values of options
+func crement_health(i):
+	total_hp = clamp(total_hp + i, 1, 5)
+	get_node(main_menu + "Health/HealthNum").text = str(total_hp)
+func crement_players(i):
+	max_players = clamp(max_players + i, 2, 8)
+	get_node(main_menu + "Players/PlayerNum").text = str(max_players)
+func crement_balls(i):
+	max_balls = clamp(max_balls + i, 1, 10)
+	get_node(main_menu + "Balls/BallNum").text = str(max_balls)
+
+# Set up game, wait for players
+func load_game():
+	var save_data = File.new()
+	save_data.open("user://save.txt", File.WRITE)
+	save_data.store_line(str(total_hp))
+	save_data.store_line(str(max_players))
+	save_data.store_line(str(max_balls))
+	save_data.close()
+	
+	playing = true
+	message.text = "Waiting for players to join..."
+	get_node("Game").show()
+	get_node("UILayer/Menu").hide()
+	get_node("Game/Camera2D").position = get_node("Game/TestMap/DefCamPos").position
+	for i in max_balls:
 		var ball = load("res://Ball.tscn").instance()
-		ball.position = spawn.position
-		balls.add_child(ball)
+		ball.position = get_node("Game/TestMap/BallSpawns").get_child(i).position
+		get_node("Game/Balls").add_child(ball)
 	randomize()
 	var map_color = Color.from_hsv((randi() % 18 * 20.0) / 360.0, 1, 1)
 	used_colors.append(map_color)
-	get_node("TestMap").modulate = map_color
+	get_node("Game/TestMap").modulate = map_color
+
+# Signal player nodes to begin
+func start_game():
+	message.text = ""
+	for p in player_db:
+		p["node"].game_began()
+	started = true
+
+# Reset and clear players/balls
+func unload_game():
+	get_node("Game/ResetTimer").stop()
+	playing = false
+	message.text = ""
+	player_db.clear()
+	for p in players.get_children():
+		p.queue_free()
+	started = false
+	get_node("Game").hide()
+	get_node("UILayer/Menu").show()
+	for b in bars.get_children():
+		b.queue_free()
+	bars.columns = 1
+	camera.position = get_node("Game/TestMap/DefCamPos").position
+	camera.zoom = Vector2(1, 1)
 
 # Create new player
 func new_player(id):
-	var player_number = player_db.size()
+	var p_num = player_db.size()
 	var new_player = load("res://Player.tscn").instance()
-	new_player.pad_id = id
-	player_db.append({"pad": id})
-	new_player.player_number = player_number
-	new_player.total_health = total_health
-	player_db[player_number]["health"] = total_health
-	randomize()
-	var new_color = Color.from_hsv((randi() % 18 * 20.0) / 360.0, 1, 1)
+	new_player.name = str(p_num)
+	new_player.pad = id
+	var new_color = used_colors[0]
 	while used_colors.has(new_color):
 		randomize()
 		new_color = Color.from_hsv((randi() % 18 * 20.0) / 360.0, 1, 1)
 	used_colors.append(new_color)
 	new_player.modulate = new_color
-	add_player_to_hud(player_number, total_health, new_color)
-	player_db[player_number]["color"] = new_color
-	new_player.spawn_position = player_spawns[player_number].position
-	new_player.spawn_rotation = player_spawns[player_number].rotation
+	var hud = add_player_to_hud(total_hp, new_color)
+	new_player.spawn_pos = player_spawns.get_child(p_num).position
+	new_player.spawn_rot = player_spawns.get_child(p_num).rotation
 	new_player.connect("hit", self, "on_player_hit")
-	player_db[player_number]["node"] = new_player
+	player_db.append({pad = id, hp = total_hp, color = new_color, hud = hud, node = new_player})
 	players.add_child(new_player)
 	players.move_child(new_player, 0)
 
-# Manage player health
-func on_player_hit(player_num):
-	var player = player_db[player_num]
-	if player["health"] <= 1:
-		player["node"].reset()
-		player["health"] = total_health
-	else:
-		player["node"].damage()
-		player["health"] -= 1
-	var hp_bits = player["hud"].get_children()
-	for i in range(hp_bits.size()):
-		hp_bits[i].modulate = Color(0.2,0.2,0.2,0.2)
-		if player["health"] > i:
-			hp_bits[i].modulate = Color(1,1,1,1)
-
 # Add player HUD
-func add_player_to_hud(player, hp, col):
+func add_player_to_hud(hp, col):
 	var new_bar = HBoxContainer.new()
 	new_bar.size_flags_horizontal = HBoxContainer.SIZE_EXPAND_FILL
 	new_bar.modulate = col
 	new_bar.alignment = BoxContainer.ALIGN_CENTER
-	var bars_wrap = VBoxContainer.new()
 	var hp_bar = HBoxContainer.new()
 	var hp_texture = TextureRect.new()
 	hp_texture.texture = load("res://img/hp-label.png")
@@ -85,11 +135,25 @@ func add_player_to_hud(player, hp, col):
 		hp_bit.texture = load("res://img/hp-bit.png")
 		hp_bits.add_child(hp_bit)
 	hp_bar.add_child(hp_bits)
-	bars_wrap.add_child(hp_bar)
-	new_bar.add_child(bars_wrap)
+	new_bar.add_child(hp_bar)
 	bars.add_child(new_bar)
-	bars.columns = clamp(bars.get_children().size(), 0, 4)
-	player_db[player]["hud"] = hp_bits
+	bars.columns = clamp(bars.get_children().size(), 1, 4)
+	return hp_bar
+
+# Manage player health
+func on_player_hit(p_num):
+	player_db[p_num]["node"].damage()
+	player_db[p_num]["hp"] -= 1
+	if player_db[p_num]["hp"] == 0:
+		player_db[p_num]["node"].queue_free()
+		if player_db[p_num]["pad"] >= 0:
+			Input.start_joy_vibration(player_db[p_num]["pad"], .2, .2, .3)
+		player_db[p_num]["hud"].get_child(0).modulate = Color(.3, .3, .3, .3)
+	var hp_bits = player_db[p_num]["hud"].get_child(1).get_children()
+	for i in range(hp_bits.size()):
+		hp_bits[i].modulate = Color(.3, .3, .3, .3)
+		if player_db[p_num]["hp"] > i:
+			hp_bits[i].modulate = Color(1, 1, 1, 1)
 
 # Checks if a pad is already used
 func is_new_pad(id):
@@ -99,8 +163,8 @@ func is_new_pad(id):
 	return true
 
 func _process(_delta):
-	# Create player if sensed input
-	if player_db.size() < max_players:
+	# Create player if sensed input, start game when players join
+	if player_db.size() < max_players and playing:
 		if Input.is_key_pressed(KEY_ENTER) and is_new_pad(-1):
 			new_player(-1)
 		elif Input.is_key_pressed(KEY_KP_ENTER) and is_new_pad(-2):
@@ -109,10 +173,18 @@ func _process(_delta):
 			for c in Input.get_connected_joypads():
 				if Input.is_joy_button_pressed(c, JOY_START) and is_new_pad(c):
 					new_player(c)
-	if Input.is_key_pressed(KEY_ESCAPE):
-		return get_tree().reload_current_scene()
+	if player_db.size() >= max_players and not started:
+		start_game()
 	
-	# Center camera to average player position
+	# Game has ended, begin unloading
+	if started and playing and players.get_child_count() < 2:
+		playing = false
+		message.text = "Game ended!"
+		for ball in get_node("Game/Balls").get_children():
+			ball.queue_free()
+		get_node("Game/ResetTimer").start(3)
+	
+	# Center camera to average player position, zoom camera to always view all players
 	if players.get_child_count() > 0:
 		var avg_x = 0
 		var avg_y = 0
@@ -122,22 +194,19 @@ func _process(_delta):
 		avg_x /= players.get_child_count()
 		avg_y /= players.get_child_count()
 		camera.position = Vector2(avg_x, avg_y)
-	
-	# Zoom camera to always view all players
-	if players.get_child_count() > 1:
-		var max_x = players.get_children()[0].position.x
-		var min_x = players.get_children()[0].position.x
-		var max_y = players.get_children()[0].position.y
-		var min_y = players.get_children()[0].position.y
+		
+		var max_x = players.get_child(0).position.x
+		var min_x = players.get_child(0).position.x
+		var max_y = players.get_child(0).position.y
+		var min_y = players.get_child(0).position.y
 		for player in players.get_children():
-			max_x = player.position.x if player.position.x > max_x else max_x
-			min_x = player.position.x if player.position.x < min_x else min_x
-			max_y = player.position.y if player.position.y > max_y else max_y
-			min_y = player.position.y if player.position.y < min_y else min_y
+			max_x = max(player.position.x, max_x)
+			min_x = min(player.position.x, min_x)
+			max_y = max(player.position.y, max_y)
+			min_y = min(player.position.y, min_y)
 		var new_zoom = Vector2.ZERO
-		var new_zoom_x = (abs(max_x - min_x) + zoom_threshold) / OS.window_size.x
-		var new_zoom_y = (abs(max_y - min_y) + zoom_threshold) / OS.window_size.y
-		new_zoom.x = new_zoom_x if new_zoom_x > new_zoom_y else new_zoom_y
-		new_zoom.y = new_zoom_x if new_zoom_x > new_zoom_y else new_zoom_y
+		var new_zoom_x = (abs(max_x - min_x) + zoom_margin) / OS.window_size.x
+		var new_zoom_y = (abs(max_y - min_y) + zoom_margin) / OS.window_size.y
+		new_zoom = Vector2(max(new_zoom_x, new_zoom_y), max(new_zoom_x, new_zoom_y))
 		new_zoom = Vector2(1, 1) if new_zoom < Vector2(1, 1) else new_zoom
-		camera.zoom = camera.zoom.linear_interpolate(new_zoom, zoom_acceleration)
+		camera.zoom = camera.zoom.linear_interpolate(new_zoom, zoom_accel)
--- Player.gd
+++ Player.gd
@@ -1,103 +1,99 @@
 extends KinematicBody2D
 
 signal hit(p)
+
+var started = false
+var pad = 0
+var stick_dz = 0.2
+
+onready var back = get_node("Back")
 onready var safe_timer = get_node("SafeTimer")
 var safe = false
 
-export var player_number = 0
-export var pad_id = 0
-var stick_dz = 0.2
-
-var velocity = Vector2.ZERO
-export var spawn_position = Vector2(0, 0)
-export var spawn_rotation = 0
-export var acceleration = 0.06
-export var deceleration = 0.02
-export var move_speed = 400
-export var sprint_speed = 550
-export var rotate_speed = 3.3
-
-export var total_health = 4
-var health = total_health
+var vel = Vector2.ZERO
+var spawn_pos = Vector2(0, 0)
+var spawn_rot = 0
+var accel = 0.06
+var decel = 0.02
+var speed = {move = 400, sprint = 550, rot = 3.3}
 
 func _ready():
 	safe_timer.start(4)
 	safe = true
-	position = spawn_position
-	rotation = spawn_rotation
+	position = spawn_pos
+	rotation = spawn_rot
+	
+	safe_timer.connect("timeout", self, "safe_ended")
+	back.connect("body_exited", self, "back_entered")
 
 func _physics_process(delta):
+	if not started:
+		return
 	var input_vel = Vector2.ZERO
 	var input_rot = 0
 	
 	# Manage inputs (-1 and -2 are keyboard controls)
-	if pad_id == -1:
+	if pad == -1:
 		input_vel.y = -float(Input.is_key_pressed(KEY_W)) + float(Input.is_key_pressed(KEY_S))
 		input_vel.x = -float(Input.is_key_pressed(KEY_A)) + float(Input.is_key_pressed(KEY_D))
 		input_rot = float(Input.is_key_pressed(KEY_H)) - float(Input.is_key_pressed(KEY_G))
 		input_vel = input_vel.normalized()
-		input_vel *= sprint_speed if Input.is_key_pressed(KEY_SHIFT) else move_speed
-		rotation += deg2rad(input_rot * rotate_speed)
-	elif pad_id == -2:
+		input_vel *= speed["sprint"] if Input.is_key_pressed(KEY_SHIFT) else speed["move"]
+		rotation += deg2rad(input_rot * speed["rot"])
+	elif pad == -2:
 		input_vel.y = -float(Input.is_key_pressed(KEY_UP)) + float(Input.is_key_pressed(KEY_DOWN))
 		input_vel.x = -float(Input.is_key_pressed(KEY_LEFT)) + float(Input.is_key_pressed(KEY_RIGHT))
 		input_rot = float(Input.is_key_pressed(KEY_KP_3)) - float(Input.is_key_pressed(KEY_KP_2))
 		input_vel = input_vel.normalized()
-		input_vel *= sprint_speed if Input.is_key_pressed(KEY_KP_1) else move_speed
-		rotation += deg2rad(input_rot * rotate_speed)
+		input_vel *= speed["sprint"] if Input.is_key_pressed(KEY_KP_1) else speed["move"]
+		rotation += deg2rad(input_rot * speed["rot"])
 	else:
-		var left_x = Input.get_joy_axis(pad_id, JOY_AXIS_0)
-		var left_y = Input.get_joy_axis(pad_id, JOY_AXIS_1)
+		var left_x = Input.get_joy_axis(pad, JOY_AXIS_0)
+		var left_y = Input.get_joy_axis(pad, JOY_AXIS_1)
 		if abs(left_x) > stick_dz or abs(left_y) > stick_dz:
 			var x_mult = 1 if left_x > 0 else -1
 			var y_mult = 1 if left_y > 0 else -1
 			input_vel = Vector2(x_mult * pow(left_x, 2), y_mult * pow(left_y, 2))
-		input_vel *= sprint_speed if Input.is_joy_button_pressed(pad_id, 6) else move_speed
-		var right_stick = Vector2(Input.get_joy_axis(pad_id, JOY_AXIS_2), Input.get_joy_axis(pad_id, JOY_AXIS_3))
+		input_vel *= speed["sprint"] if Input.is_joy_button_pressed(pad, 6) else speed["move"]
+		var right_stick = Vector2(Input.get_joy_axis(pad, JOY_AXIS_2), Input.get_joy_axis(pad, JOY_AXIS_3))
 		if right_stick.length() > 0.7:
 			rotation += get_angle_to(position + right_stick) * 0.1
 	
 	# Smoothify movement
 	if input_vel.length() > 0:
-		velocity = velocity.linear_interpolate(input_vel, acceleration)
+		vel = vel.linear_interpolate(input_vel, accel)
 	else:
-		velocity = velocity.linear_interpolate(Vector2.ZERO, deceleration)
+		vel = vel.linear_interpolate(Vector2.ZERO, decel)
 	
-	# Manage collisions with balls
-	var collision = move_and_collide(velocity * delta, false)
-	if collision:
-		if collision.collider.is_in_group("balls"):
-			if velocity.length() > 0:
-				collision.collider.apply_central_impulse(-collision.normal * velocity.length())
+	# Manage colls with balls
+	var coll = move_and_collide(vel * delta, false)
+	if coll and started:
+		if coll.collider.is_in_group("balls"):
+			if vel.length() > 0:
+				coll.collider.apply_central_impulse(-coll.normal * vel.length())
 			else:
-				collision.collider.apply_central_impulse(-collision.normal)
-			velocity = velocity.bounce(collision.normal).normalized()
+				coll.collider.apply_central_impulse(-coll.normal)
+			vel = vel.bounce(coll.normal).normalized()
 		else:
-			velocity = velocity.bounce(collision.normal)
-		if pad_id >= 0:
-			Input.start_joy_vibration(pad_id, 0.2, 0.2, 0.1)
+			vel = vel.bounce(coll.normal)
+		if pad >= 0:
+			Input.start_joy_vibration(pad, 0.2, 0.2, 0.1)
 
 # Manage player damage, invincibiility, and resetting
-func _on_back_entered(body):
+func back_entered(body):
 	if body.is_in_group("balls") and not safe:
-		emit_signal("hit", player_number)
+		emit_signal("hit", int(name))
 
-# Reset player to defaults
-func reset():
-	velocity = Vector2.ZERO
-	position = spawn_position
-	health = total_health
-	if pad_id >= 0:
-		Input.start_joy_vibration(pad_id, 0.2, 0.2, 0.3)
-	safe = true
-	safe_timer.start(4)
+# Game has begun, allow player movement
+func game_began():
+	started = true
 
 # Damage player
 func damage():
-	health -= 1
 	safe = true
 	safe_timer.start(2)
 
-func _on_SafeTimer_timeout():
-	safe_timer.stop()
+# Remove invincibility
+func safe_ended():
 	safe = false
+	safe_timer.stop()
